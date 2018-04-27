f88d5142556fc87da8eef3e702f176d1070bf528
tangixnmao <1051477653@qq.com>
2018-04-27 15:29:56 +0800

    dgfdffdfghghghghghghghghghghghghghggg

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,102 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.web.context.ContextLoader;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.security.KeyPair;
+
 @SpringBootApplication
-public class DemoApplication implements WebMvcConfigurer {
+@EnableAuthorizationServer
+public class DemoApplication implements WebMvcConfigurer,ResourceServerConfigurer,AuthorizationServerConfigurer,WebSecurityConfigurer{
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @Override
+    public void init(SecurityBuilder securityBuilder) throws Exception {
+
+    }
+
+    @Override
+    public void configure(SecurityBuilder securityBuilder) throws Exception {
+
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        KeyPair keyPair = new KeyStoreKeyFactory(
+                new ClassPathResource("keystore.jks"), "txmtxm".toCharArray())
+                .getKeyPair("txm");
+        converter.setKeyPair(keyPair);
+        return converter;
+    }
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/oauth/confirm_access").setViewName("authorize");
     }
+
+
+
+
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer authorizationServerSecurityConfigurer) throws Exception {
+
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clientDetailsServiceConfigurer) throws Exception {
+        clientDetailsServiceConfigurer.inMemory()
+                .withClient("acme")
+                .secret("acmesecret")
+                .authorizedGrantTypes("authorization_code", "refresh_token",
+                        "password").scopes("openid");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer authorizationServerEndpointsConfigurer) throws Exception {
+        authorizationServerEndpointsConfigurer.authenticationManager(new OAuth2AuthenticationManager()).accessTokenConverter(
+                jwtAccessTokenConverter());
+    }
+
+
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resourceServerSecurityConfigurer) throws Exception {
+
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin().loginPage("/login").permitAll().and().authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+
 }
